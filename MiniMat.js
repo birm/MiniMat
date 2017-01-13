d43a3bd872e47b928fb5e3c07a68d2416c5fe97b
Birm <birm@rbirm.us>
2017-01-13 00:43:04 -0500

    update number of tests

--- MiniMat.js
+++ MiniMat.js
@@ -269,7 +269,7 @@ var test = require('tape')
 
 // manually make a 2x2 with [1,2,3,4]
 test( 'default inits test', function(t) {
-    t.plan(17);
+    t.plan(19);
 
 
     t.doesNotThrow( function() {
